2fc393710cc4fe3397fdd1d2af35d93a6b721d00
Devin Egger <devinegger@Devins-Mac-mini.home>
2022-03-30 19:05:22 -0600

    working woocommerce products

--- functions.php
+++ functions.php
@@ -130,3 +130,98 @@ function team_member_place_holder($title , $post){
 
 
 // add custom fields to product
+
+// Woocommerce custom fields for General tab
+// Display Fields
+add_action( 'woocommerce_product_options_general_product_data', 'woo_add_custom_general_fields' );
+
+// Save Fields
+add_action( 'woocommerce_process_product_meta', 'woo_add_custom_general_fields_save' );
+
+// Adding New Fields
+function woo_add_custom_general_fields() {
+
+  global $woocommerce, $post;
+  
+  echo '<div class="options_group">';
+  
+  // Custom fields will be created here...
+  
+// Start Text Field
+woocommerce_wp_text_input( 
+	array( 
+		'id'          => '_text_field', 
+		'label'       => __( 'INCI name', 'woocommerce' ), 
+	)
+);
+// End Text Field
+
+// Start Textarea
+woocommerce_wp_textarea_input( 
+	array( 
+		'id'          => '_textarea', 
+		'label'       => __( 'Product description', 'woocommerce' ), 
+	)
+);
+// End Textarea
+  
+// Start Number Field
+woocommerce_wp_text_input( 
+	array( 
+		'id'                => '_number_field', 
+		'label'             => __( 'Product code', 'woocommerce' ), 
+		'type'              => 'number', 
+		'custom_attributes' => array(
+				'step' 	=> 'any',
+				'min'	=> '0'
+			) 
+	)
+);
+// End Number Field
+
+  echo '</div>';
+	
+}
+
+  // Saving Fields Values
+function woo_add_custom_general_fields_save( $post_id ){
+	
+	// Text Field
+	$woocommerce_text_field = $_POST['_text_field'];
+	if( !empty( $woocommerce_text_field ) )
+		update_post_meta( $post_id, '_text_field', esc_attr( $woocommerce_text_field ) );
+		
+	// Number Field
+	$woocommerce_number_field = $_POST['_number_field'];
+	if( !empty( $woocommerce_number_field ) )
+		update_post_meta( $post_id, '_number_field', esc_attr( $woocommerce_number_field ) );
+		
+	// Textarea
+	$woocommerce_textarea = $_POST['_textarea'];
+	if( !empty( $woocommerce_textarea ) )
+		update_post_meta( $post_id, '_textarea', esc_html( $woocommerce_textarea ) );
+	
+	/*  I don't know where these are coming from, and I don't see them
+	 *   in the product data, so I'm not going to include them for now 
+	 * 
+	
+	 // Select
+	$woocommerce_select = $_POST['_select'];
+	if( !empty( $woocommerce_select ) )
+		update_post_meta( $post_id, '_select', esc_attr( $woocommerce_select ) );
+	
+	// Custom Field
+	$custom_field_type =  array( esc_attr( $_POST['_field_one'] ), esc_attr( $_POST['_field_two'] ) );
+	update_post_meta( $post_id, '_custom_field_type', $custom_field_type );
+	
+	// Hidden Field
+	$woocommerce_hidden_field = $_POST['_hidden_field'];
+	if( !empty( $woocommerce_hidden_field ) )
+		update_post_meta( $post_id, '_hidden_field', esc_attr( $woocommerce_hidden_field ) );
+		
+	// Product Field Type
+	$product_field_type =  $_POST['product_field_type'];
+	update_post_meta( $post_id, '_product_field_type_ids', $product_field_type );
+	*/
+	
+}
--- template-parts/acf-product-selection-list.php
+++ template-parts/acf-product-selection-list.php
@@ -14,22 +14,39 @@ $product_items = get_sub_field('product_list'); // array of Products
         <?php 
         foreach($product_items as $product_item) : 
 
-            $product = $product_item['product']; // this is the Product Post Object
+            $product_post = $product_item['product']; // this is the Product Post Object
 
-            $product_ID = $product->ID;
-            $product_name = $product->post_title;
-            $product_content = $product->post_content;
+            $product_ID = $product_post->ID;
+            $product_name = $product_post->post_title;
+
+            $product = wc_get_product($product_ID);
+
+            //print_r($product);
 
             $product_image_ID = get_post_thumbnail_id( $product_ID ); 
-            $product_image = wp_get_attachment_image( $product_image_ID, array(260,180), FALSE, array('class'=>'img-full') ); // not sure how to grab the product url and alt from here... 
+            $product_image = wp_get_attachment_image( $product_image_ID, array(260,180), FALSE, array('class'=>'img-full') ); // not sure how to grab the product img url and alt from here... 
 
-            //echo $product_image;
+            // product custom meta
+            $INCI_name = get_post_meta($product_ID, '_text_field', TRUE);
+            $product_number = get_post_meta($product_ID, '_number_field', TRUE);
+            $product_description = get_post_meta($product_ID, '_textarea', TRUE);
+
+            //echo $product->get_type();
+
             
-            // use these if there are custom fields for the product
-            // $job_title = get_post_meta($team_member->ID, 'job_title', TRUE);
-            // $job_description = get_post_meta( $team_member->ID, 'job_description', TRUE );
+            if ($product->is_type( 'variable' )) {
+                $available_variations = $product->get_available_variations();
 
-            // print_r($product);
+                /*
+                foreach ($available_variations as $variation) {
+                    $pa_request_name = $variation['attributes']['attribute_pa_request'];
+                    echo ucwords(str_replace('-', ' ', $pa_request_name));
+                    //print_r($variation);
+                }
+                */
+            }
+
+            
 
         ?>
         <div class="row bg-light mb-2 p-3 product-tile">
@@ -40,20 +57,31 @@ $product_items = get_sub_field('product_list'); // array of Products
                     </div>
                     <div class="col-md-9 p-2">
                         <h3 class="fw-bold mb-0"><?= $product_name ?></h3>
-                        <span class="product-sub-titles text-gray">INCI Name: Orbignya Oleifera Seed Oil and Tocopherol</span>
-                        <p class="text-black"><?= $product_content ?></p>
-                        <span class="text-black">#125845</span>
+                        <span class="product-sub-titles text-gray">INCI Name: <?= $INCI_name ?></span>
+                        <p class="text-black"><?= $product_description ?></p>
+                        <span class="text-black">#<?= $product_number ?></span>
                     </div>
                 </div>
-                <div class="row">
+
+                <? foreach($available_variations as $variation) :?>
+                    <? //print_r($variation); ?>
+                    <?php $pa_request_name = $variation['attributes']['attribute_pa_request']; ?>
+                    <?php $variation_ID = $variation['variation_id']; ?>
+                    <?php // create the add to cart url ?>
+                    <?php $base_url = get_home_url() . '/cart/?add-to-cart=' . $product_ID; ?>
+                    <?php $variation_url = '&variation_id=' . $variation_ID . '&attribute_pa_request=' . $pa_request_name; ?>
+                    <?php $quantity_url = '&quantity=1'; ?>
+                    <?php $add_to_cart_url =  $base_url . $variation_url . $quantity_url; ?>
+                <div class="row py-1">
                     <div class="col">
-                        <form class="text-end">
-                            <label class="text-gray">Request Standard Sample</label>
+                        <div class="text-end">
+                            <label class="text-gray"><?= ucwords(str_replace('-', ' ', $pa_request_name)); ?></label>
                             <input class="mx-2 text-center" placeholder="1" size="2"/>
-                            <button class="btn btn-secondary rounded-0" type="submit">Add to Cart</button>
-                        </form>
+                            <a class="btn btn-secondary rounded-0" href="<?= $add_to_cart_url ?>">Add to Cart</a>
+                        </div>
                     </div>
                 </div>
+                <? endforeach ; ?>
             </div>
         </div>
         <?php endforeach; ?>
